b1171f776bbc70fb5beebe8a5f67bb36b814a50a
Zachary Bunch <zacharybunch1284@gmail.com>
2024-06-25 15:16:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,173 +9,168 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 app.use(bodyParser.json());
-
-// Serve static files from the 'build' directory
 app.use(express.static(path.join(__dirname, 'build')));
 
-// Add the privacy route before the catch-all route ('*')
 app.get('/privacy', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'privacy.html'));
 });
 
-// The '/:room' route
 app.get('/:room', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Catch-all route for other routes, serve the 'index.html' file
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const users = {};
 let activeRooms = {};
-const roomCanvases = {}; // Store canvas state for each room
-const drawingStates = {}; // Declare drawingStates variable here
+const roomCanvases = {};
+const drawingStates = {};
 const chatMessages = {};
 const activeUsers = {};
-const roomUserActions = {}; // { room: { userId: [actions] } }
-const userRedoStacks = {}; // { room: { userId: [redoActions] } }
+const roomUserActions = {};
+const userRedoStacks = {};
 const cursors = {};
+const userDrawingHistory = {};
+const userUndoStacks = {};
 
-let isDrawing = false;
-
-const INACTIVITY_TIMEOUT = 10 * 60 * 1000; // 10 minutes in milliseconds
+const INACTIVITY_TIMEOUT = 10 * 60 * 1000;
 
 io.on('connection', (socket) => {
     activeUsers[socket.id] = Date.now();
     
     io.emit('activeRooms', { activeRooms });
-    
-    // Function to check and handle inactivity
+
     const checkInactivity = () => {
         const now = Date.now();
         const lastActivity = activeUsers[socket.id];
         if (now - lastActivity > INACTIVITY_TIMEOUT) {
-            // User has been inactive for more than 10 minutes
-            // Emit a message to the client about being kicked for being AFK
             socket.emit('kickedForAFK', { message: 'You have been kicked for being inactive.' });
-
-            // Disconnect the socket
             socket.disconnect(true);
         }
     };
     
     socket.on('join', (data) => {
-        const { room, userId, username, color } = data;
+            const { room, userId, username, color } = data;
 
-        socket.join(room);
+            socket.join(room);
 
-        users[socket.id] = { username, color, room };
+            users[socket.id] = { username, color, room };
 
-        if (!roomUserActions[room]) {
-            roomUserActions[room] = {};
-        }
+            if (!roomUserActions[room]) {
+                roomUserActions[room] = {};
+            }
 
-        if (!roomUserActions[room][userId]) {
-            roomUserActions[room][userId] = [];
-        }
+            if (!roomUserActions[room][userId]) {
+                roomUserActions[room][userId] = [];
+            }
 
-        if (!userRedoStacks[room]) {
-            userRedoStacks[room] = {};
-        }
+            if (!userRedoStacks[socket.id]) {
+                userRedoStacks[socket.id] = [];  // Ensure redo stack is initialized
+            }
 
-        if (!userRedoStacks[room][userId]) {
-            userRedoStacks[room][userId] = [];
-        }
+            if (!userUndoStacks[socket.id]) {
+                userUndoStacks[socket.id] = [];  // Ensure undo stack is initialized
+            }
 
-        activeUsers[socket.id] = Date.now();
+            activeUsers[socket.id] = Date.now();
 
-        // Initialize room details if not already done
-        if (!activeRooms[room]) {
-            activeRooms[room] = {
-                isPrivate: true, // Initialize the privacy setting as false by default
-                count: 0 // Initialize user count
+            if (!activeRooms[room]) {
+                activeRooms[room] = {
+                    isPrivate: true,
+                    count: 0
+                };
+            }
+
+            activeRooms[room].count++;
+
+            if (!roomCanvases[room]) {
+                roomCanvases[room] = [];
+            }
+
+            if (roomCanvases[room]) {
+                roomCanvases[room].forEach(item => {
+                    socket.emit(item.type, { ...item.data, userId: item.userId });
+                });
+            }
+
+            if (chatMessages[data.room]) {
+                chatMessages[data.room].forEach((message) => {
+                    io.to(socket.id).emit('message', message);
+                });
+            }
+
+            if (!drawingStates[data.room]) {
+                drawingStates[data.room] = {};
+            }
+
+            drawingStates[data.room][socket.id] = {
+                drawing: false,
+                lastX: 0,
+                lastY: 0,
             };
-        }
 
-        // Increment the user count for the joined room
-        activeRooms[room].count++;
+            io.in(room).emit('activeUsersCount', { room, count: activeRooms[room].count });
 
-        // Send existing canvas data to the newly joined user
-        if (roomCanvases[room]) {
-            roomCanvases[room].forEach(item => {
-                socket.emit(item.type, { ...item.data, userId: item.userId });
+            io.to(data.room).emit('message', {
+                username: 'System',
+                color: 'white',
+                message: `<span style="color: ${color}">${username}</span> has joined the room!`,
             });
-        }
 
-        // Always send previous chat messages to the newly joined user
-        if (chatMessages[data.room]) {
-            chatMessages[data.room].forEach((message) => {
-                io.to(socket.id).emit('message', message);
-            });
-        }
+            const systemMessage = {
+                username: 'System',
+                color: 'white',
+                message: `<span style="color: ${color}">${username}</span> has joined the room!`,
+            };
 
-        // Initialize drawing state for the user in the room
-        if (!drawingStates[data.room]) {
-            drawingStates[data.room] = {};
-        }
+            chatMessages[data.room] = chatMessages[data.room] || [];
+            chatMessages[data.room].push(systemMessage);
 
-        drawingStates[data.room][socket.id] = {
-            drawing: false,
-            lastX: 0,
-            lastY: 0,
-        };
-
-        // Emit the updated user count to all clients in the room
-        io.in(room).emit('activeUsersCount', { room, count: activeRooms[room].count });
-
-        io.to(data.room).emit('message', {
-            username: 'System',
-            color: 'white',
-            message: `<span style="color: ${color}">${username}</span> has joined the room!`,
+            socket.emit('updatePrivacy', { isPrivate: activeRooms[room].isPrivate });
         });
-
-        // Store the system message for future users
-        const systemMessage = {
-            username: 'System',
-            color: 'white',
-            message: `<span style="color: ${color}">${username}</span> has joined the room!`,
-        };
-
-        chatMessages[data.room] = chatMessages[data.room] || [];
-        chatMessages[data.room].push(systemMessage);
-
-        // Emit the privacy setting only to the newly joined user
-        socket.emit('updatePrivacy', { isPrivate: activeRooms[room].isPrivate });
-    });
     
     socket.on('message', (data) => {
         io.to(data.room).emit('message', data);
         io.emit('activeRooms', { activeRooms });
         
-        // Store chat messages for each room
         chatMessages[data.room] = chatMessages[data.room] || [];
         chatMessages[data.room].push(data);
         
-        // Update user's last activity timestamp when they send a message
         activeUsers[socket.id] = Date.now();
     });
     
-    const inactivityCheckInterval = setInterval(checkInactivity, 1000); // Check every second
-    
+    const inactivityCheckInterval = setInterval(checkInactivity, 1000);
+
     socket.on('drawingStart', (data) => {
-        const { room, startX, startY, color, mode } = data;
+            const { room, startX, startY, color, mode } = data;
 
-        if (!drawingStates[room]) {
-            drawingStates[room] = {};
-        }
+            if (!userDrawingHistory[socket.id]) {
+                userDrawingHistory[socket.id] = [];
+                userUndoStacks[socket.id] = [];
+                userRedoStacks[socket.id] = [];
+            }
 
-        if (!roomCanvases[room]) {
-            roomCanvases[room] = [];
-        }
+            if (!roomCanvases[room]) {
+                roomCanvases[room] = [];
+            }
 
-        roomCanvases[room].push({ userId: socket.id, type: 'drawingStart', data: { startX, startY, mode, color } });
+            const action = { userId: socket.id, type: 'drawingStart', data: { startX, startY, mode, color } };
+            roomCanvases[room].push(action);
+            userDrawingHistory[socket.id].push([action]); // Initialize a new action array
 
-        drawingStates[room][socket.id] = { drawing: true, path: [{ x: startX, y: startY }], mode };
+            // Clear redo stack on new action
+            userRedoStacks[socket.id] = [];
 
-        io.to(room).emit('drawingStart', { userId: socket.id, startX, startY, mode, color });
-    });
+            if (!drawingStates[room]) {
+                drawingStates[room] = {};
+            }
+
+            drawingStates[room][socket.id] = { drawing: true, path: [{ x: startX, y: startY }], mode };
+
+            io.to(room).emit('drawingStart', { userId: socket.id, startX, startY, mode, color });
+        });
 
     socket.on('drawing', (data) => {
         const { room, x, y, color, mode } = data;
@@ -184,13 +179,15 @@ io.on('connection', (socket) => {
             return;
         }
 
-        drawingStates[room][socket.id].path.push({ x, y });
-
         if (!roomCanvases[room]) {
             roomCanvases[room] = [];
         }
 
-        roomCanvases[room].push({ userId: socket.id, type: 'drawing', data: { x, y, color, mode } });
+        drawingStates[room][socket.id].path.push({ x, y });
+
+        const action = { userId: socket.id, type: 'drawing', data: { x, y, color, mode } };
+        roomCanvases[room].push(action);
+        userDrawingHistory[socket.id][userDrawingHistory[socket.id].length - 1].push(action); // Add to the current action
 
         io.to(room).emit('drawing', { userId: socket.id, x, y, color, mode });
     });
@@ -202,15 +199,21 @@ io.on('connection', (socket) => {
             return;
         }
 
-        drawingStates[room][socket.id].drawing = false;
-
         if (!roomCanvases[room]) {
             roomCanvases[room] = [];
         }
 
-        roomCanvases[room].push({ userId: socket.id, type: 'drawingEnd' });
+        drawingStates[room][socket.id].drawing = false;
+
+        const action = { userId: socket.id, type: 'drawingEnd' };
+        roomCanvases[room].push(action);
+        userDrawingHistory[socket.id][userDrawingHistory[socket.id].length - 1].push(action); // Add to the current action
 
         io.to(room).emit('drawingEnd', { userId: socket.id });
+
+        // Push the complete action to the undo stack
+        userUndoStacks[socket.id] = userUndoStacks[socket.id] || [];
+        userUndoStacks[socket.id].push(userDrawingHistory[socket.id].pop());
     });
 
     socket.on('clearCanvas', (data) => {
@@ -218,7 +221,7 @@ io.on('connection', (socket) => {
         roomCanvases[room] = [];
         io.to(room).emit('clearCanvas');
     });
-    
+
     socket.on('cursorMove', (data) => {
         const user = users[socket.id];
         if(user) {
@@ -267,12 +270,6 @@ io.on('connection', (socket) => {
             socket.emit('noAvailableRooms', { message: "No available public rooms. Try creating a new one or wait for public rooms to become available." });
         }
     });
-
-    socket.on('updateDrawingHistory', (drawingHistory) => {
-        const { room } = users[socket.id];
-        roomUserActions[room][socket.id] = drawingHistory;
-        io.to(room).emit('drawingHistory', drawingHistory);
-    });
     
     socket.on('disconnect', () => {
         const user = users[socket.id];
@@ -319,10 +316,52 @@ io.on('connection', (socket) => {
 
         delete activeUsers[socket.id];
     });
+
+    socket.on('undo', (data) => {
+           const user = users[socket.id];
+           const room = user.room;
+
+           if (userUndoStacks[socket.id] && userUndoStacks[socket.id].length > 0) {
+               const actions = userUndoStacks[socket.id].pop();
+               userRedoStacks[socket.id] = userRedoStacks[socket.id] || [];
+               userRedoStacks[socket.id].push(actions);
+
+               // Remove the actions from the room canvas
+               actions.forEach(action => {
+                   const index = roomCanvases[room].indexOf(action);
+                   if (index !== -1) {
+                       roomCanvases[room].splice(index, 1);
+                   }
+               });
+
+               // Broadcast the new canvas state to the room
+               io.to(room).emit('updateCanvas', { drawingHistory: roomCanvases[room] });
+           }
+       });
+
+       socket.on('redo', (data) => {
+           const user = users[socket.id];
+           const room = user.room;
+
+           if (userRedoStacks[socket.id] && userRedoStacks[socket.id].length > 0) {
+               const actions = userRedoStacks[socket.id].pop();
+               userUndoStacks[socket.id] = userUndoStacks[socket.id] || [];
+               userUndoStacks[socket.id].push(actions);
+
+               // Add the actions back to the room canvas
+               actions.forEach(action => {
+                   roomCanvases[room].push(action);
+               });
+
+               // Broadcast the new canvas state to the room
+               io.to(room).emit('updateCanvas', { drawingHistory: roomCanvases[room] });
+           }
+       });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
 
